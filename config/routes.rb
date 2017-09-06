a984e83388d955016d87d21fced4d4572b867535
Q. Zhang <qczhang18@gmail.com>
2017-09-06 11:50:43 -0400

    namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :dog_hobbies
-  resources :hobbies
-  resources :dogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :dogs, except: [:new, :edit]
+      resources :hobbies, except: [:new, :edit]
+    end
+  end
 end
